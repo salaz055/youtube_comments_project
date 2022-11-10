b5ea94f671d9ddee356ce5bf2c5f607b031f30ea
Salaz <salaz055.ds@gmail.com>
2022-11-10 11:57:48 -0600

    first commit

---
+++ secrets.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 11:49:50 2022
+
+@author: salaz
+"""
+
+import os
+os.chdir(r'C:\Users\salaz\OneDrive\Desktop\Projects\youtube_comments_project')
+print(os.getcwd())
+
+youtube_key = 'AIzaSyCP7RMrbDiASPD7Vx8tmCbXqUUBGkvQc1g'
